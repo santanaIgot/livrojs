3e8727e09d7185b786acad5105533944daae6fdd
santanaigot <santanaigoor01@gmail.com>
2025-11-13 00:20:50 -0300

    exercicio B

---
+++ cap02/exercicios/b/js/exB.js
@@ -0,0 +1,26 @@
+
+const form = document.querySelector('form')
+const resp1 = document.querySelector('#inShow')
+
+console.log(form);
+
+
+form.addEventListener("submit", (e) => {
+
+
+    e.preventDefault()
+
+
+    const isValueUse = Number(form.inValue.value);
+    const isTimeUse = Number(form.inTime.value);
+
+    if(isTimeUse > 15){
+        const total = isValueUse + isValueUse;
+        resp1.innerText = `O valor a pagar é ${total}`
+    }
+
+
+    
+    
+    
+})
